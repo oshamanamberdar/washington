ac04ad8b83d74bcc9f2e4d0c2995ea2417a1a266
oshamanamberdar <oshama.namberdar@sbsolutionsnepal.com>
2022-08-26 23:36:44 +0545

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {NbCardModule, NbIconModule, NbStatusService, NbThemeModule, NbThemeService} from "@nebular/theme";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NgbActiveModal, NgbDatepickerModule} from "@ng-bootstrap/ng-bootstrap";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbCardModule,
+    ReactiveFormsModule,
+    NgbDatepickerModule,
+    NbThemeModule,
+    HttpClientModule,
+    TranslateModule.forRoot( {
+      loader: {
+        provide : TranslateLoader,
+        useFactory : HttpLoaderFactory,
+        deps : [HttpClient]
+      }
+    })
+
+
   ],
-  providers: [],
+  providers: [NbStatusService, NgbActiveModal],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal,
+              private translate: TranslateService) { }
+
 
   ngOnInit(): void {
+  }
+
+  openLoginCard(content: any) {
+    this.modalService.open(content);
+  }
+
+  openCreateAccountCard(content: any) {
+    this.modalService.open(content)
+  }
+
+  openAboutThisSite(content: any) {
+    this.modalService.open(content);
+  }
+
+  translateToSpanish() {
+    this.translate.use('es');
   }
 
 }
